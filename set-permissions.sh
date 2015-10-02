8bb1315ff2f1fdf1deaa271a2ac7c59365eae301
Patrick Mahoney <mahonpj@gmail.com>
2015-10-02 02:38:20 -0400

    Removed unused permission script

--- set-permissions.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-sudo chown pmahoney:www-data .
-sudo chown -R pmahoney:www-data *
-find . -type f \( ! -path "./bin/*"  -or  ! -name "set-permissions.sh" \) | xargs chmod 664
-find . -type f \( -path "./bin/*"  -or  -name "set-permissions.sh" \) | xargs chmod 775
-find . -type d | xargs chmod 775
-find . -type d | xargs chmod +s
-umask 0002
